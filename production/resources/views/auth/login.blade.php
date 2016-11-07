c3757b57ce8b28114801010e06d262184c9553fc
Eric Sun <Eric Sun>
2016-11-07 22:29:46 +0800

    Standardized the size of buttons and inputs of Register and Login Page

--- production/resources/views/auth/login.blade.php
+++ production/resources/views/auth/login.blade.php
@@ -24,50 +24,60 @@
 					</h1>
 				</div>
 			</div>
-			<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} ui one centered column grid">
+			
+			<div class="five wide column">
+			<div class="ui grid centered aligned"> <!-- These 3 are used so that the buttons and inputs can be the same width as the standard width -->
+			<div class="ten wide column">
+			
+				<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
 
-				<div class="ui input max-input multi-space">
-					<input placeholder="Name" id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+					<div class="ui fluid input multi-space">
+						<input placeholder="Name" id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
-					@if ($errors->has('email'))
-						<span class="help-block">
-							<strong>{{ $errors->first('email') }}</strong>
-						</span>
-					@endif
+						@if ($errors->has('email'))
+							<span class="help-block">
+								<strong>{{ $errors->first('email') }}</strong>
+							</span>
+						@endif
+					</div>
 				</div>
-			</div>
 
-			<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }} ui one centered column grid">
+				<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
 
-				<div class="ui input max-input multi-space">
-					<input placeholder="Password" id="password" type="password" class="form-control" name="password" required>
+					<div class="ui input fluid">
+						<input placeholder="Password" id="password" type="password" class="form-control" name="password" required>
 
-					@if ($errors->has('password'))
-						<span class="help-block">
-							<strong>{{ $errors->first('password') }}</strong>
-						</span>
-					@endif
+						@if ($errors->has('password'))
+							<span class="help-block">
+								<strong>{{ $errors->first('password') }}</strong>
+							</span>
+						@endif
+					</div>
 				</div>
-			</div>
 
-			<div class="form-group ui one centered column grid multi-space">
-				<div class="ui checkbox row">
-						<input type="checkbox" name="remember"> 
-						<label>Remember Me</label>
+				<div class="form-group grid multi-space">
+					<div class="ui checkbox row">
+							<input type="checkbox" name="remember"> 
+							<label>Remember Me</label>
+					</div>
 				</div>
-			</div>
 
-			<div class="form-group ui one centered column grid multi-space">
-				<div class="col-md-8 col-md-offset-4 row">
-					<button type="submit" class="ui button btn btn-primary">
-						Login
-					</button>
+				<div class="form-group multi-space">
+					<div class="row">
+						<button type="submit" class="ui fluid button btn btn-primary">
+							Login
+						</button>
 
-					<a class="btn btn-link" href="{{ url('/password/reset') }}">
-						Forgot Your Password?
-					</a>
+						<a class="btn btn-link" href="{{ url('/password/reset') }}">
+							Forgot Your Password?
+						</a>
+					</div>
 				</div>
+				
 			</div>
+			</div>
+			</div>
+			
 		</form>
 	</div>
 	</body>
--- production/resources/views/auth/register.blade.php
+++ production/resources/views/auth/register.blade.php
@@ -14,71 +14,79 @@
 	
 	<body>
 	<div class="register-master-div">
-		<div>
-			<div class="seven wide column grid">
-				<h1 class="head-font title-logo">
-					<span class="color-yellow">W</span><span class="color-red">I</span><span class="color-blue">L</span><span class="color-green">D</span>
-					CARD
-				</h1>
+		<div class="seven wide column grid">
+			<h1 class="head-font title-logo">
+				<span class="color-yellow">W</span><span class="color-red">I</span><span class="color-blue">L</span><span class="color-green">D</span>
+				CARD
+			</h1>
+		</div>
+		
+		<div class="five wide column">
+		<div class="ui grid centered aligned"> <!-- These 3 are used so that the buttons and inputs can be the same width as the standard width -->
+		<div class="ten wide column">
+		
+			<div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+
+				<div class="ui input fluid multi-space">
+					<input placeholder="Name" id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+
+					@if ($errors->has('name'))
+						<span class="help-block">
+							<strong>{{ $errors->first('name') }}</strong>
+						</span>
+					@endif
+				</div>
 			</div>
-		</div>
-		<div class="form-group{{ $errors->has('name') ? ' has-error' : '' }} ui one centered column grid">
 
-			<div class="ui input max-input multi-space">
-				<input placeholder="Name" id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+			<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
 
-				@if ($errors->has('name'))
-					<span class="help-block">
-						<strong>{{ $errors->first('name') }}</strong>
-					</span>
-				@endif
+				<div class="ui input fluid">
+					<input placeholder="Email" id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+					@if ($errors->has('email'))
+						<span class="help-block">
+							<strong>{{ $errors->first('email') }}</strong>
+						</span>
+					@endif
+				</div>
 			</div>
-		</div>
 
-		<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} ui one centered column grid">
+			<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
 
-			<div class="ui input max-input">
-				<input placeholder="Email" id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+				<div class="ui input fluid multi-space">
+					<input placeholder="Password" id="password" type="password" class="form-control" name="password" required>
 
-				@if ($errors->has('email'))
-					<span class="help-block">
-						<strong>{{ $errors->first('email') }}</strong>
-					</span>
-				@endif
+					@if ($errors->has('password'))
+						<span class="help-block">
+							<strong>{{ $errors->first('password') }}</strong>
+						</span>
+					@endif
+				</div>
 			</div>
-		</div>
 
-		<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }} ui one centered column grid">
+			<div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
 
-			<div class="ui input max-input multi-space">
-				<input placeholder="Password" id="password" type="password" class="form-control" name="password" required>
+				<div class="ui input fluid go-away-regbtn">
+					<input placeholder="Confirm Password" id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
 
-				@if ($errors->has('password'))
-					<span class="help-block">
-						<strong>{{ $errors->first('password') }}</strong>
-					</span>
-				@endif
+					@if ($errors->has('password_confirmation'))
+						<span class="help-block">
+							<strong>{{ $errors->first('password_confirmation') }}</strong>
+						</span>
+					@endif
+				</div>
 			</div>
-		</div>
 
-		<div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }} ui one centered column grid">
-
-			<div class="ui input max-input go-away-regbtn input-space">
-				<input placeholder="Confirm Password" id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-
-				@if ($errors->has('password_confirmation'))
-					<span class="help-block">
-						<strong>{{ $errors->first('password_confirmation') }}</strong>
-					</span>
-				@endif
+			<div class="form-group multi-space">
+				<button type="submit" class="btn btn-primary ui fluid button max-button">
+					Register
+				</button>
 			</div>
-		</div>
 
-		<div class="form-group ui one centered column grid multi-space">
-			<button type="submit" class="btn btn-primary ui button max-button">
-				Register
-			</button>
 		</div>
+		</div>
+		</div>
+		
 	</div>
 	</body>
 </html>
