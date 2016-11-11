1f1e00840e2fc90846ece6dc2c49a2edfa97efbe
Meta-kJ <k.jimg22@gmail.com>
2016-11-12 02:19:57 +0800

    Update DBUpdate.sql

--- Database/DBUpdate.sql
+++ Database/DBUpdate.sql
@@ -9,19 +9,28 @@ CREATE TABLE users(
 id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 email varchar(255) NOT NULL ,
 password varchar(255) NOT NULL,
+first_name varchar(255),
+last_name varchar(255),
+age int,
 remember_token varchar(100),
 updated_at TIMESTAMP,
 created_at TIMESTAMP
 );
 
-DROP TABLE IF EXISTS profiles;
-CREATE TABLE profiles(
+DROP TABLE IF EXISTS availPref;
+CREATE TABLE availPref(
 id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-account_id int,
-FOREIGN KEY(account_id) REFERENCES users(id),
-first_name varchar(255),
-last_name varchar(255),
-age int,
+preference_id int,
+FOREIGN KEY(preference_id) REFERENCES preferences(id),
+time_stamped TIMESTAMP,
+updated_at TIMESTAMP,
+created_at TIMESTAMP
+);
+
+DROP TABLE IF EXISTS categories;
+CREATE TABLE categories(
+id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name varchar(255),
 updated_at TIMESTAMP,
 created_at TIMESTAMP
 );
@@ -30,16 +39,8 @@ DROP TABLE IF EXISTS preferences;
 CREATE TABLE preferences(
 id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 name varchar(255),
-updated_at TIMESTAMP,
-created_at TIMESTAMP
-);
-
-DROP TABLE IF EXISTS categories;
-CREATE TABLE categories(
-id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-user_id int,
-FOREIGN KEY(user_id) REFERENCES users(id),
-name varchar(255),
+category_id int,
+FOREIGN KEY(category_id) REFERENCES categories(id)
 updated_at TIMESTAMP,
 created_at TIMESTAMP
 );
@@ -52,17 +53,16 @@ id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 name varchar(255),
 rating int,
 description varchar(255),
+popularity int,
 updated_at TIMESTAMP,
 created_at TIMESTAMP
 );
 
-DROP TABLE IF EXISTS preferenceCategoriesSuggestions;
+DROP TABLE IF EXISTS preferenceSuggestion;
 CREATE TABLE preferenceSuggestions(
 id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 preference_id int,
 FOREIGN KEY(preference_id) REFERENCES preferences(id),
-category_id int,
-FOREIGN KEY(category_id) REFERENCES categories(id),
 suggestion_id int,
 FOREIGN KEY(suggestion_id) REFERENCES suggestions(id),
 updated_at TIMESTAMP,
@@ -85,9 +85,13 @@ updated_at TIMESTAMP,
 created_at TIMESTAMP
 );
 
-DROP TABLE IF EXISTS timeTrackers;
-CREATE TABLE timeTrackers(
+DROP TABLE IF EXISTS bookmarks;
+CREATE TABLE bookmarks(
 id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+account_id int,
+FOREIGN KEY(account_id) REFERENCES accounts(id),
+category_id int,
+FOREIGN KEY(category_id) REFERENCES categories(id)
 updated_at TIMESTAMP,
 created_at TIMESTAMP
 );
