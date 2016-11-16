683b423668d3801397601d2e72f1fa3cd8ab5d7e
Eric Sun <Eric Sun>
2016-11-16 16:50:18 +0800

    Detail page started

---
+++ production/resources/views/detail.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+			<meta charset="utf-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+
+			<title>{{ config('app.name') }}</title>
+			
+			<link href="/css/semantic.css" rel="stylesheet">
+			<link href="/css/style.css" rel="stylesheet">
+			<script src="/js/functionality.js">
+			</script>
+	</head>
+	
+	<body>
+	<div class="detail-master-div"> <!-- Detail Master Div-->
+			
+		<div class="ui left aligned grid title-mini-bar"><!-- Wild Card Title Bar -->
+			<h1 class="head-font title-logo w-push">
+				<span class="color-yellow title-mini">W</span><span class="color-red title-mini">I</span><span class="color-blue title-mini">L</span><span class="color-green title-mini">D</span>
+				<span class="title-mini">CARD</span>
+			</h1>
+		</div>
+		
+	</div>
+	</body>
+	
+<html>
