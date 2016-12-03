13456c659866d13283083261cf1aca8603886fa5
Eric Sun <Eric Sun>
2016-12-03 22:19:07 +0800

    Comment Controller Update

--- production/app/Http/Controllers/CommentController.php
+++ production/app/Http/Controllers/CommentController.php
@@ -10,10 +10,17 @@ class CommentController extends Controller
 {
     //This shall be used to get all them comments for a specific suggestion
 	public function getComments($suggestionID){
-		$comments = Comment::find($suggestionID);
+		$comments = Comment::where('suggestion_id',$suggestionID)->get();
+		foreach($comments as $comment){ // as of now, only echos its content, possible others to echo: time updates/created, user
+			echo $comment->content;
+		}
 	}
 	
-	public function giveComment($suggestionID){
-		
+	public function giveComment($suggestionID,$content){
+		$newComment = new Comment;
+		$newComment->suggestion_id = $suggestionID;
+		$newComment->content = $content;
+		$newComment->created_at = date("Y-m-d H:i:s"); // Get immediate date and time
+		$newComment->save();
 	}
 }
