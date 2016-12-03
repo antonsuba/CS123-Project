abfe03416f49d80dc4e9f14c480e1f3ab91c453b
Fiel Faustino <thaf.faustino@gmail.com>
2016-12-04 00:37:20 +0800

    Fix Activity-Suggestion Relationship

--- production/app/Activity.php
+++ production/app/Activity.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Activity extends Model
 {
     public function suggestion(){
-        return $this->belongsToMany('App\Suggestion');
+        return $this->belongsTo('App\Suggestion');
     }
 
     public function place(){
