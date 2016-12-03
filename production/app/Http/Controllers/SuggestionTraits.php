f964e3ad3853dc06b773a7814b6345b0d5840502
Fiel Faustino <thaf.faustino@gmail.com>
2016-12-04 00:27:50 +0800

    Fixed Traits

--- production/app/Http/Controllers/SuggestionTraits.php
+++ production/app/Http/Controllers/SuggestionTraits.php
@@ -4,33 +4,7 @@ namespace App\Http\Controllers;
 use App\Activity;
 use App\Place;
 use App\Location;
-
-trait ActivityTraits {
-
-    public function createActivity($suggestionID, $description, $placeName, $locationName) {
-            $activity = new Activity;
-            $activity->suggestion_id = $suggestionID;
-            $activity->description = $description;
-            $activity->save();
-
-            $location = Location::where('name', $locationName)->first();
-            $RowCount = Place::where(['name' => $placeName, 'location_id' => $location->id])->get()->count();
-            if(RowCount > 0){
-                $place = Place::where('name', $placeName)->first();
-            } else{
-                $place = new Place;
-                $place->name = $placeName;
-                //$place->description = $placeArray[1];
-                $place->location_id = $location->id;
-                $place->save();
-            }
-            $activity->place_id = $place->id;
-
-            return $activity;
-    }
-
-
-}
+use App\Suggestion;
 
 trait SuggestionTraits{
     public function getSuggestionDetails($suggestionID){
@@ -58,8 +32,8 @@ trait SuggestionTraits{
         $i = 0;
         $places = array();
         foreach ($activities as $activity) {
-            $place = $activity->place();
-            $places[i] = $place;
+            $place = $activity->place()->first();
+            $places[$i] = $place;
             $i++;
         }
 
--- production/app/User.php
+++ production/app/User.php
@@ -28,6 +28,6 @@ class User extends Authenticatable
     ];
 	
 	public function comment(){
-		return this->hasMany('App\Comment');
+		return $this->hasMany('App\Comment');
 	}
 }
