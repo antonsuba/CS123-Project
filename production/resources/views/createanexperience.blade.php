816af306fd72773321e81e87ab6df82f855540f3
Eric Sun <Eric Sun>
2016-12-06 19:50:26 +0800

    There is an upload image interface for creating suggestions, error though

--- production/resources/views/createanexperience.blade.php
+++ production/resources/views/createanexperience.blade.php
@@ -79,12 +79,24 @@
 
             <!-- Upload Image Div -->
             <div class="seven wide column">
+				<!--
                 <img id="img-upload-box" class="ui large image" src="https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s640x640/sh0.08/e35/11925616_1047949228550818_1910383083_n.jpg"/>
+				-->
+				<form id="img-upload-box" action="" method="post" enctype="multipart/form-data">
+					<!-- Image taken from: happysock.eu -->
+					<div id="image_preview"><img id="previewing" src="http://happysock.eu/wp-content/themes/mt-four/assets/images/no-img.jpg"/></div>
+						<hr id="line">
+						<div id="selectImage">
+						<label>Select Your Image</label><br/>
+						<input type="file" name="file" id="file" required />
+						<input type="submit" value="Upload" class="submit" />
+					</div>
+				</form>
             </div>
 
         </div>
 		
-		<div id="map" style="height:50%;width:50%;"></div>
+		<div id="map" style="height:50%;width:100%;"></div>
     
     </div>
 
@@ -112,6 +124,54 @@ $(document).ready(function(){
         $('#img-upload-box').show();
         $("#prompt-header").html("Create an experience for others to try out");
     });
+	
+	// Used for the image upload
+	$("#uploadimage").on('submit',(function(e) {
+		e.preventDefault();
+		$("#message").empty();
+		$('#loading').show();
+		$.ajax({
+			url: "imageHelper.php", // Url to which the request is send
+			type: "POST",             // Type of request to be send, called as method
+			data: new FormData(this), // Data sent to server, a set of key/value pairs (i.e. form fields and values)
+			contentType: false,       // The content type used when sending data to the server.
+			cache: false,             // To unable request pages to be cached
+			processData:false,        // To send DOMDocument or non processed data file it is set to false
+			success: function(data)   // A function to be called if request succeeds
+			{
+				$('#loading').hide();
+				$("#message").html(data);
+			}
+		});
+	}));
+	
+	$(function() {
+		$("#file").change(function() {
+			$("#message").empty(); // To remove the previous error message
+			
+			var file = this.files[0];
+			var imagefile = file.type;
+			var match= ["image/jpeg","image/png","image/jpg"];
+			
+			if(!((imagefile==match[0]) || (imagefile==match[1]) || (imagefile==match[2]))){
+				$('#previewing').attr('src','noimage.png');
+				$("#message").html("<p id='error'>Please Select A valid Image File</p>"+"<h4>Note</h4>"+"<span id='error_message'>Only jpeg, jpg and png Images type allowed</span>");
+				return false;
+			}else{
+				var reader = new FileReader();
+				reader.onload = imageIsLoaded;
+				reader.readAsDataURL(this.files[0]);
+			}
+		});
+	});
+	function imageIsLoaded(e) {
+		$("#file").css("color","green");
+		$('#image_preview').css("display", "block");
+		$('#previewing').attr('src', e.target.result);
+		$('#previewing').attr('width', '250px');
+		$('#previewing').attr('height', '230px');
+	};
+	
 });
 
 </script>
